d743149344dcb5cbaec4aedf3a99f16d430ad799
gislainejessica <gislainehjessica@gmail.com>
2020-10-13 16:04:42 -0300

    feat: map page

--- web/src/pages/OrphanateMap.tsx
+++ web/src/pages/OrphanateMap.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+import logo from '../images/Logo.svg';
 
+import '../styles/pages/OrphanateMap.css';
+// import { TileLayer } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-const OrphanateMap: React.FC = () => {
+function OrphanateMap() {
   return (
-    <h1> Orfanatos </h1>
+    <div id="page-map">
+      <aside >
+        <header>
+          <img src={logo} alt="HappY" />
+          <h2> Escolha um orfanato no Mapa </h2>
+          <p> Muitas crianças estão esparando a sua visita :) </p>
+        </header>
+
+        <footer>
+          <strong> Serra </strong>
+          <span> Espiríto Santo </span>
+        </footer>
+
+      </aside>
+
+      <Map
+        center={[-20.165736, -40.2578524]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      </Map>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#ffffff" />
+      </Link>
+
+    </div>
   );
 }
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
-import Orphan from './pages/OrphanateMap';
+import OrphanateMap from './pages/OrphanateMap';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/app" component={Orphan} />
+        <Route path="/app" component={OrphanateMap} />
       </Switch>
     </BrowserRouter>
   )
