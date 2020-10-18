ce0c357600cb216e0f8232e3fedfa3f2edf08cbe
gislainejessica <gislainehjessica@gmail.com>
2020-10-18 12:13:29 -0300

    add bd to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,19 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
+
+app.use(express.json())
+
+app.get('/users', (request, response) => {
+  return response.json({ message: ' Hello NLW' })
+})
+
+
+app.post('/orphanages', (request, response) => {
+  const { name, latitude, longitude, about, instructions, open_on_weekends, opening_hours } = request.body
+
+  return response.json({ message: ' Hello NLW' })
+})
 
 app.listen(3333);
